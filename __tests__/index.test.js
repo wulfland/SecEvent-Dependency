ecb0824e1cbd006d8dd32c915fcd5ef8e595058e
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2024-05-07 11:23:13 +0200

    Initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,11 @@
+/**
+ * Unit tests for the index.js
+ */
+// index.test.js
+describe('index.js', () => {
+  it('greet function returns Hello world!', () => {
+    const greet = require('../src/index')
+
+    expect(greet()).toBe('Hello world! v2.1.2')
+  })
+})
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// index.js
+module.exports = function greet () {
+  return 'Hello world! v2.1.2'
+}
